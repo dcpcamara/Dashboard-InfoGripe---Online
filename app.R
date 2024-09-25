439cf9d8097f6e1f87051f2f69c847ace9500e7a
Daniel Câmara <dcpcamara@gmail.com>
2024-09-25 10:01:42 -0300

    Update app.R
    Corrected issue with punctuation and accentuation

--- app.R
+++ app.R
@@ -1,5 +1,6 @@
 # Load necessary library
 library(shiny)
+library(stringi)
 
 # Define the UI for the application
 ui <- fluidPage(
@@ -39,8 +40,12 @@ ui <- fluidPage(
   
   # Include the logo and title in the same row
   fluidRow(
-    column(1, img(src = "https://yourdomain.com/logo.png", class = "logo-banner")),  # Replace with your logo URL
-    column(11, div("Dashboard de resultados do boletim InfoGripe", class = "title-panel"))  # Title in the second column
+    column(1, 
+           img(src = "logo.png", 
+               class = "logo-banner")),  # Logo in the first column
+    column(11, 
+           div("Dashboard de resultados do boletim InfoGripe", 
+               class = "title-panel"))  # Title in the second column
   ),
   
   # Create a tab layout
@@ -66,30 +71,36 @@ ui <- fluidPage(
              h3("Figura 1. Série temporal, estimativa de casos recentes de SRAG e tendências de curto e longo prazo em todo o território nacional - geral e por faixas etárias de interesse.", 
                 class = "figure-title"),
              fluidRow(
-               column(6, uiOutput("brazil_figure1")),
-               column(6, uiOutput("brazil_figure2"))
+               column(6, 
+                      uiOutput("brazil_figure1")),
+               column(6, 
+                      uiOutput("brazil_figure2"))
              ),
              
              # Figure 3 alone with a header
              h3("Figura 2. Incidência e mortalidade nas últimas 8 semanas.", 
                 class = "figure-title"),
              fluidRow(
-               column(12, uiOutput("brazil_figure3"))
+               column(12, 
+                      uiOutput("brazil_figure3"))
              ),
              
              # Figures 4 and 5 side by side with a header
              h3("Figura 3. Média da incidência e mortalidade semanal de SRAG nas últimas oito semanas, por vírus e faixa etária de interesse.",
                 class = "figure-title"),
              fluidRow(
-               column(6, uiOutput("brazil_figure4")),
-               column(6, uiOutput("brazil_figure5"))
+               column(6, 
+                      uiOutput("brazil_figure4")),
+               column(6, 
+                      uiOutput("brazil_figure5"))
              ),
              
              # Figure 6 alone at the bottom
              h3("Figura 4. Incidência semanal de SRAG e por vírus identificado laboratorialmente, por faixas etárias de interesse.", 
                 class = "figure-title"),
              fluidRow(
-               column(12, uiOutput("brazil_figure6"))
+               column(12, 
+                      uiOutput("brazil_figure6"))
              )
     ),
     
@@ -107,8 +118,10 @@ ui <- fluidPage(
              h3("Figura 1. Série temporal, estimativa de casos recentes de SRAG e tendências de curto e longo prazo por Unidade Federativa.", 
                 class = "figure-title"),
              fluidRow(
-               column(6, uiOutput("state_image1")),
-               column(6, uiOutput("state_image2"))
+               column(6, 
+                      uiOutput("state_image1")),
+               column(6, 
+                      uiOutput("state_image2"))
              ),
              h3("Figura 2. Casos semanais de SRAG por vírus identificado.", 
                 class = "figure-title"), 
@@ -131,8 +144,10 @@ ui <- fluidPage(
              h3("Série temporal, estimativa de casos recentes de SRAG e tendências por Capital.", 
                 class = "figure-title"),
              fluidRow(
-               column(6, uiOutput("capital_image1")),
-               column(6, uiOutput("capital_image2"))
+               column(6, 
+                      uiOutput("capital_image1")),
+               column(6, 
+                      uiOutput("capital_image2"))
              )
     )
   )
@@ -141,43 +156,59 @@ ui <- fluidPage(
 # Define server logic
 server <- function(input, output) {
   
-  ### Tab 1: Trends Figures ###
+  ### Tab 1: Trends Figures
   output$trends_figure1 <- renderUI({
-    tags$img(src = "https://gitlab.fiocruz.br/marcelo.gomes/infogripe/-/raw/master/Boletins%20do%20InfoGripe/Imagens/UF/Mapa_ufs_tendencia.png", class = "responsive-img", alt = "Trends Figure 1")
+    tags$img(src = "https://gitlab.fiocruz.br/marcelo.gomes/infogripe/-/raw/master/Boletins%20do%20InfoGripe/Imagens/UF/Mapa_ufs_tendencia.png", 
+             class = "responsive-img", 
+             alt = "Trends Figure 1")
   })
   
   output$trends_figure2 <- renderUI({
-    tags$img(src = "https://gitlab.fiocruz.br/marcelo.gomes/infogripe/-/raw/master/Boletins%20do%20InfoGripe/Imagens/Capitais/Mapa_capitais_tendencia.png", class = "responsive-img", alt = "Trends Figure 2")
+    tags$img(src = "https://gitlab.fiocruz.br/marcelo.gomes/infogripe/-/raw/master/Boletins%20do%20InfoGripe/Imagens/Capitais/Mapa_capitais_tendencia.png", 
+             class = "responsive-img", 
+             alt = "Trends Figure 2")
   })
   
-  ### Tab 2: Brazil Static Figures ###
+  ### Tab 2: Brazil Static Figures
   output$brazil_figure1 <- renderUI({
-    tags$img(src = "https://gitlab.fiocruz.br/marcelo.gomes/infogripe/-/raw/master/Boletins%20do%20InfoGripe/Imagens/UF/fig_BR.png", class = "responsive-img", alt = "Figure 1 for Brazil")
+    tags$img(src = "https://gitlab.fiocruz.br/marcelo.gomes/infogripe/-/raw/master/Boletins%20do%20InfoGripe/Imagens/UF/fig_BR.png", 
+             class = "responsive-img", 
+             alt = "Brazil Figure 1")
   })
   
   output$brazil_figure2 <- renderUI({
-    tags$img(src = "https://gitlab.fiocruz.br/marcelo.gomes/infogripe/-/raw/master/Boletins%20do%20InfoGripe/Imagens/UF/fig_BR_fx_etaria.png", class = "responsive-img", alt = "Figure 2 for Brazil")
+    tags$img(src = "https://gitlab.fiocruz.br/marcelo.gomes/infogripe/-/raw/master/Boletins%20do%20InfoGripe/Imagens/UF/fig_BR_fx_etaria.png", 
+             class = "responsive-img", 
+             alt = "Brazil Figure 2")
   })
   
   output$brazil_figure3 <- renderUI({
-    tags$img(src = "https://gitlab.fiocruz.br/marcelo.gomes/infogripe/-/raw/master/Boletins%20do%20InfoGripe/Imagens/UF/fig_BR_inc_mort.png", class = "responsive-img", alt = "Figure 3 for Brazil")
+    tags$img(src = "https://gitlab.fiocruz.br/marcelo.gomes/infogripe/-/raw/master/Boletins%20do%20InfoGripe/Imagens/UF/fig_BR_inc_mort.png", 
+             class = "responsive-img", 
+             alt = "Brazil Figure 3")
   })
   
   output$brazil_figure4 <- renderUI({
-    tags$img(src = "https://gitlab.fiocruz.br/marcelo.gomes/infogripe/-/raw/master/Boletins%20do%20InfoGripe/Imagens/UF/fig_BR_virus_lab_inc.png", class = "responsive-img", alt = "Figure 4 for Brazil")
+    tags$img(src = "https://gitlab.fiocruz.br/marcelo.gomes/infogripe/-/raw/master/Boletins%20do%20InfoGripe/Imagens/UF/fig_BR_virus_lab_inc.png", 
+             class = "responsive-img", 
+             alt = "Brazil Figure 4")
   })
   
   output$brazil_figure5 <- renderUI({
-    tags$img(src = "https://gitlab.fiocruz.br/marcelo.gomes/infogripe/-/raw/master/Boletins%20do%20InfoGripe/Imagens/UF/fig_BR_virus_lab_mort.png", class = "responsive-img", alt = "Figure 5 for Brazil")
+    tags$img(src = "https://gitlab.fiocruz.br/marcelo.gomes/infogripe/-/raw/master/Boletins%20do%20InfoGripe/Imagens/UF/fig_BR_virus_lab_mort.png", 
+             class = "responsive-img", 
+             alt = "Brazil Figure 5")
   })
   
   output$brazil_figure6 <- renderUI({
-    tags$img(src = "https://gitlab.fiocruz.br/marcelo.gomes/infogripe/-/raw/master/Boletins%20do%20InfoGripe/Imagens/UF/fig_BR_virus_lab.png", class = "responsive-img", alt = "Figure 6 for Brazil")
+    tags$img(src = "https://gitlab.fiocruz.br/marcelo.gomes/infogripe/-/raw/master/Boletins%20do%20InfoGripe/Imagens/UF/fig_BR_virus_lab.png", 
+             class = "responsive-img", 
+             alt = "Brazil Figure 6")
   })
   
-  ### Tab 3: Brazilian States ###
+  ### Tab 3: Brazilian States
   get_image_url <- function(state, image_number) {
-    base_url <- "https://gitlab.fiocruz.br/marcelo.gomes/infogripe/-/raw/master/Boletins%20do%20InfoGripe/Imagens/UF/"  # Replace with your base URL
+    base_url <- "https://gitlab.fiocruz.br/marcelo.gomes/infogripe/-/raw/master/Boletins%20do%20InfoGripe/Imagens/UF/"
     if (image_number == 1) {
       return(paste0(base_url, "fig_", state, ".png"))
     } else if (image_number == 2) {
@@ -189,33 +220,43 @@ server <- function(input, output) {
   
   output$state_image1 <- renderUI({
     state <- input$selected_state
-    tags$img(src = get_image_url(state, 1), class = "responsive-img", alt = paste("State", state, "Image 1"))
+    tags$img(src = get_image_url(state, 1), 
+             class = "responsive-img", 
+             alt = paste("UF", state, "Image 1"))
   })
   
   output$state_image2 <- renderUI({
     state <- input$selected_state
-    tags$img(src = get_image_url(state, 2), class = "responsive-img", alt = paste("State", state, "Image 2"))
+    tags$img(src = get_image_url(state, 2), 
+             class = "responsive-img", 
+             alt = paste("UF", state, "Image 2"))
   })
   
   output$state_image3 <- renderUI({
     state <- input$selected_state
-    tags$img(src = get_image_url(state, 3), class = "responsive-img", alt = paste("State", state, "Image 3"))
+    tags$img(src = get_image_url(state, 3), 
+             class = "responsive-img", 
+             alt = paste("UF", state, "Image 3"))
   })
   
-  ### Tab 4: Brazilian Capitals ###
+  ### Tab 4: Brazilian Capitals
   get_capital_image_url <- function(capital, image_number) {
     # Map capital names to state abbreviations
-    state_abbr <- switch(capital,
-                         "Rio Branco" = "AC", "Maceió" = "AL", "Macapá" = "AP", "Manaus" = "AM",
-                         "Salvador" = "BA", "Fortaleza" = "CE", "Brasília" = "DF", "Vitória" = "ES",
-                         "Goiânia" = "GO", "São Luís" = "MA", "Cuiabá" = "MT", "Campo Grande" = "MS",
-                         "Belo Horizonte" = "MG", "Belém" = "PA", "João Pessoa" = "PB", "Curitiba" = "PR",
-                         "Recife" = "PE", "Teresina" = "PI", "Rio de Janeiro" = "RJ", "Natal" = "RN",
-                         "Porto Alegre" = "RS", "Porto Velho" = "RO", "Boa Vista" = "RR", "Florianópolis" = "SC",
-                         "São Paulo" = "SP", "Aracaju" = "SE", "Palmas" = "TO")
+    state_abbr <- switch(
+      capital,
+      "Rio Branco" = "AC", "Maceió" = "AL", "Macapá" = "AP", "Manaus" = "AM",
+      "Salvador" = "BA", "Fortaleza" = "CE", "Brasília" = "DF", "Vitória" = "ES",
+      "Goiânia" = "GO", "São Luís" = "MA", "Cuiabá" = "MT", "Campo Grande" = "MS",
+      "Belo Horizonte" = "MG", "Belém" = "PA", "João Pessoa" = "PB", "Curitiba" = "PR",
+      "Recife" = "PE", "Teresina" = "PI", "Rio de Janeiro" = "RJ", "Natal" = "RN",
+      "Porto Alegre" = "RS", "Porto Velho" = "RO", "Boa Vista" = "RR", "Florianópolis" = "SC",
+      "São Paulo" = "SP", "Aracaju" = "SE", "Palmas" = "TO"
+    )
     
-    base_url <- "https://gitlab.fiocruz.br/marcelo.gomes/infogripe/-/raw/master/Boletins%20do%20InfoGripe/Imagens/UF/"  # Replace with your base URL
-    capital_formatted <- gsub(" ", "_", toupper(capital))
+    base_url <- "https://gitlab.fiocruz.br/marcelo.gomes/infogripe/-/raw/master/Boletins%20do%20InfoGripe/Imagens/Capitais/"
+    capital_formatted <- gsub(" ", 
+                              "_", 
+                              toupper(stri_trans_general(capital, "Latin-ASCII")))
     
     if (image_number == 1) {
       return(paste0(base_url, "fig_", state_abbr, "_", capital_formatted, ".png"))
@@ -226,12 +267,16 @@ server <- function(input, output) {
   
   output$capital_image1 <- renderUI({
     capital <- input$selected_capital
-    tags$img(src = get_capital_image_url(capital, 1), class = "responsive-img", alt = paste("Capital", capital, "Image 1"))
+    tags$img(src = get_capital_image_url(capital, 1), 
+             class = "responsive-img", 
+             alt = paste("Capital", capital, "Image 1"))
   })
   
   output$capital_image2 <- renderUI({
     capital <- input$selected_capital
-    tags$img(src = get_capital_image_url(capital, 2), class = "responsive-img", alt = paste("Capital", capital, "Image 2"))
+    tags$img(src = get_capital_image_url(capital, 2), 
+             class = "responsive-img", 
+             alt = paste("Capital", capital, "Image 2"))
   })
 }
 
