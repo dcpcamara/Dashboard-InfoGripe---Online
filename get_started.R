57cb2671df421f36414bdf4d8ea6bdd583bfd0db
Daniel Câmara <dcpcamara@gmail.com>
2024-10-02 15:23:28 -0300

    Uploading to shinyapps.io

--- get_started.R
+++ get_started.R
@@ -1,13 +1,6 @@
-shiny::runApp()
-shiny::runApp()
-library(shiny); runApp('teste.R')
-runApp('teste.R')
-runApp('teste.R')
-runApp('teste.R')
-runApp('teste.R')
-runApp('teste.R')
-shiny::runApp()
 install.packages('rsconnect')
+
 rsconnect::setAccountInfo(name='danielcamara', token='D0DE5C7B8104A8A20240E7C981559534', secret='C580Xl0MimrspsgMI21fUSDs+v8VQ/gfXzF0MDjU')
+
 library(rsconnect)
 rsconnect::deployApp('../Dashboard InfoGripe - Online/')
